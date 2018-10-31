fb9cbfea8a66e1dfeff531fc66dbaa3fbcd92fc6
itsshailesh47 <itsshailesh47@gmail.com>
2018-10-31 09:18:05 +0530

    updated
    updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tokyo'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
